c7a5d71e11782fd4167c14541ea45a1ad3b45bd8
Jacob Morra <jacob.morra@uoit.net>
2015-11-01 01:24:27 -0500

    Update send.php

--- send.php
+++ send.php
@@ -1,12 +1,15 @@
 <?php
-	if (isset($_POST["submit"])){
+	if (isset($_POST["email"]) && isset($_POST["submit"])){
 		$to      = 'jmorra@hotmail.com';
-		$subject = 'the subject';
-		$message = 'hello';
+		$subject = 'message from ' . $_POST["name"];
+		$message = $_POST["comment"];
 		$headers = 'From: jacobmorra.com' . "\r\n" .
 			'Reply-To: webmaster@example.com' . "\r\n" .
 			'X-Mailer: PHP/' . phpversion();
 
 		mail($to, $subject, $message, $headers);
+		
+		echo "Thank you " . $_POST["name"] . ", your message has been sent.";
 	}
+	
 ?>
