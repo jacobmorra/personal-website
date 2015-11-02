6748dc7cf029ff45f269d28487d7d8ecf2dca915
Jacob Morra <jacob.morra@uoit.net>
2015-11-02 00:26:44 -0500

    Update send.php

--- send.php
+++ send.php
@@ -1,15 +1,24 @@
 <?php
-	if (isset($_POST["email"]) && isset($_POST["submit"])){
+	if (!empty($_POST["name"]) && !empty($_POST["email"])&& isset($_POST["submit"])){
 		$to      = 'jmorra@hotmail.com';
 		$subject = 'message from ' . $_POST["name"];
-		$message = $_POST["comment"];
-		$headers = 'From: jacobmorra.com' . "\r\n" .
-			'Reply-To: webmaster@example.com' . "\r\n" .
+		$message = $_POST["name"] . " wrote the following: \n" 
+		. $_POST["comment"] . "\n This person can be reached at " . $_POST["email"];
+		$headers = 'From: ' . $_POST["name"] . "\r\n" .
+			'Reply-To: jacobmorra.com' . "\r\n" .
 			'X-Mailer: PHP/' . phpversion();
 
 		mail($to, $subject, $message, $headers);
-		
-		echo "Thank you " . $_POST["name"] . ", your message has been sent.";
+		$cond = true;
 	}
-	
+	else if ((empty($_POST["name"]))&& !empty($_POST["email"])&& isset($_POST["submit"])){
+		$cond1 = true;
+	}
+	else if (!empty($_POST["name"]) && (empty($_POST["email"]))&& isset($_POST["submit"])){
+		$cond2 = true;
+	}
+	else if (empty($_POST["name"]) && (empty($_POST["email"]))&& isset($_POST["submit"])){
+		unset($_POST["submit"]);
+		$cond3 = true;		
+	}
 ?>
